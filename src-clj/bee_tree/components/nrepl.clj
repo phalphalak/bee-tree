3184d05d4b66ef9cfafc20908a0d1e54b558dd61
Roman Flammer <phalphalak@gmail.com>
2014-03-30 00:32:27 +0100

    Get browser repl to work and remove redundant repl component.

--- src-clj/bee_tree/components/nrepl.clj
+++ src-clj/bee_tree/components/nrepl.clj
@@ -1,13 +1,17 @@
 (ns bee-tree.components.nrepl
   (:require [clojure.tools.nrepl.server :as nrepl]
+            [cemerick.piggieback :as piggie]
             [com.stuartsierra.component :as component]))
 
+; (require '[cljs.repl.browser])
+; (cemerick.piggieback/cljs-repl :repl-env (cljs.repl.browser/repl-env :port 5000))
 (defrecord NRepl [port]
   component/Lifecycle
   (start [this]
     (let [port (get this :port)]
       (println (format "start nrepl on port %s" port))
-      (assoc this :server (nrepl/start-server :port port))))
+      (assoc this :server (nrepl/start-server :handler (nrepl/default-handler #'piggie/wrap-cljs-repl)
+                                              :port port))))
   (stop [this]
     (println "stop nrepl")
     (nrepl/stop-server (get this :server))
--- src-clj/bee_tree/core.clj
+++ src-clj/bee_tree/core.clj
@@ -3,7 +3,6 @@
   (:require [clojure.tools.namespace.repl :refer (refresh)]
             [bee-tree.components
              [nrepl :as nrepl]
-             [clj-repl :as clj-repl]
              [jetty :as jetty]]
             [bee-tree.routes :as routes]
             [bee-tree.middleware.view :refer [wrap-view]]
@@ -21,12 +20,10 @@
 
 (defn init []
   (nrepl/init)
-  (clj-repl/init)
   (jetty/init #'app))
 
 (defn start []
   (nrepl/start)
-  (clj-repl/start)
   (jetty/start))
 
 (defn go []
@@ -35,7 +32,6 @@
 
 (defn stop []
   (jetty/stop)
-  (clj-repl/stop)
   (nrepl/stop))
 
 (defn reset []
